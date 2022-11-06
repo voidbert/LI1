2fb7a5bf0a5e65d95b25af44f57864a2852dd185
a104348 <a104348@alunos.uminho.pt>
2022-11-06 23:36:08 +0000

    Animação de todas as linhas de um mapa (T3)

--- src/Tarefa3_2022li1g012.hs
+++ src/Tarefa3_2022li1g012.hs
@@ -26,5 +26,81 @@ module Tarefa3_2022li1g012 where
 
 import LI12223
 
+{-|
+  'velocidadeTerreno' devolve a velocidade com que se move uma linha com um
+  dado tipo de terreno.
+
+  === Exemplos
+
+  >>> velocidadeTerreno Relva
+  0
+
+  >>> velocidadeTerreno (Rio 4)
+  4
+-}
+velocidadeTerreno :: Terreno -- ^ Qualquer tipo de terreno de uma linha
+                  -> Int     -- ^ A sua velocidade
+velocidadeTerreno Relva       = 0
+velocidadeTerreno (Rio n)     = n
+velocidadeTerreno (Estrada n) = n
+
+{-|
+  'animaLinha' devolve a linha resultante após o seu movimento numa jogada.
+  Obstáculos em terrenos com velocidade @n@, movem-se @n@ posições para a
+  direita ou para a esquerda, caso @n@ seja positivo ou negativo,
+  respetivamente. Quando, no seu movimento, um obstáculo sai das bordas da
+  linha é colocado no início da mesma (mapa circular).
+
+  === Notas
+
+  Esta função suporta velocidades superiores ao comprimento da linha, graças a
+  aritmética modular (como os elementos ressurgem na linha, por exemplo, mover
+  uma linha com 5 obstáculos 5 vezes para a direita é equivalente a ficar-se
+  com a mesma linha).
+
+  Complexidade: \( O(n) \)
+
+  === Exemplos
+
+  >>> animaLinha 6 (Rio 2, [Tronco, Tronco, Nenhum, Nenhum, Tronco, Nenhum])
+  (Rio 2, [Tronco, Nenhum, Tronco, Tronco, Nenhum, Nenhum])
+
+  >>> animaLinha 4 (Estrada (-1), [Carro, Carro, Nenhum, Carro])
+  (Estrada (-1), [Carro, Nenhum, Carro, Carro])
+
+  >>> animaLinha 4 (Estrada (-5), [Carro, Carro, Nenhum, Carro])
+  (Estrada (-5), [Carro, Nenhum, Carro, Carro])
+-}
+animaLinha :: Largura                -- ^ Largura do mapa
+           -> (Terreno, [Obstaculo]) -- ^ Linha a ser animada
+           -> (Terreno, [Obstaculo]) -- ^ Linha no seu estado seguinte
+animaLinha l (t, os)
+  | vt <  0 = let (a, d) = splitAt v       os in (t, d ++ a)
+  | vt == 0 = (t, os)
+  | vt >  0 = let (a, d) = splitAt (l - v) os in (t, d ++ a)
+  where vt = velocidadeTerreno t
+        v = mod (abs vt) l
+
+{-|
+  'animaMapa' move todas as linhas de um mapa __válido__ com recurso a
+  'animaLinha'.
+
+  === Notas
+
+  Complexidade: \( O(n \times m) \), \(n\) linhas de largura \(m\)
+
+  === Exemplo
+
+  >>> animaMapa (Mapa 4 [ (Estrada 1, [Carro, Carro, Nenhum, Nenhum]),
+                          (Rio (-1), [Tronco, Nenhum, Tronco, Tronco]) ])
+  Mapa 4 [ (Estrada 1, [Nenhum,Carro,Carro,Nenhum]),
+           (Rio (-1), [Nenhum,Tronco,Tronco,Tronco] )]
+-}
+animaMapa :: Mapa
+          -> Mapa
+animaMapa (Mapa l []) = Mapa l []
+animaMapa (Mapa l (x:xs)) = Mapa l ((animaLinha l x) : n)
+  where (Mapa _ n) = animaMapa (Mapa l xs)
+
 animaJogo :: Jogo -> Jogada -> Jogo
 animaJogo = undefined
