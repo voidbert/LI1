6a16d4ca005b008eaa9ea33b2591ef7aabde93d6
a104541 <a104541@aluno.uminho.pt>
2022-11-07 23:03:05 +0000

    Testes Unitários Tarefa4

--- tests/Tarefa4_2022li1g012_Spec.hs
+++ tests/Tarefa4_2022li1g012_Spec.hs
@@ -20,5 +20,33 @@ import LI12223
 import Tarefa4_2022li1g012
 import Test.HUnit
 
+-- Testes para colisão de obstáculos em diferentes linhas/terrenos:
+-- Estrada:
+
+to1 = "TesteEstradaFalse" ~: False ~=? jogoTerminou (Jogo (Jogador (1,1)) (Mapa 3 [(Relva,[Nenhum,Arvore,Arvore]),(Estrada 1,[Carro,Nenhum,Carro])]))
+to2 = "TesteEstradaTrue"  ~: True  ~=? jogoTerminou (Jogo (Jogador (0,2)) (Mapa 3 [(Estrada 1,[Carro,Nenhum,Carro]),(Relva,[Nenhum,Arvore,Arvore])]))
+
+-- Rio:
+
+to3 = "TesteRioTrue"      ~: True  ~=? jogoTerminou (Jogo (Jogador (0,0)) (Mapa 3 [(Rio 1,[Nenhum,Nenhum,Tronco]),(Estrada 1,[Carro,Nenhum,Carro])]))
+to4 = "TesteRioFalse"     ~: False ~=? jogoTerminou (Jogo (Jogador (2,1)) (Mapa 3 [(Estrada 1,[Carro,Nenhum,Carro]),(Rio 2,[Nenhum,Nenhum,Tronco])]))
+
+-- Relva:
+
+to5 = "TesteRelvaFalse"   ~: False ~=? jogoTerminou (Jogo (Jogador (0,0)) (Mapa 3 [(Relva,[Nenhum,Arvore,Arvore]),(Estrada 1,[Carro,Nenhum,Carro])]))
+to6 = "TesteRelvaFalsel1" ~: False ~=? jogoTerminou (Jogo (Jogador (0,1)) (Mapa 3 [(Estrada 1,[Carro,Nenhum,Carro]),(Relva,[Nenhum,Arvore,Arvore])]))
+
+-- Testes para quando o jogador se encontra fora do mapa:
+-- Por Cima/Baixo:
+
+to7 = "ForaMapaporBaixo"  ~: True  ~=? jogoTerminou (Jogo (Jogador (0,2)) (Mapa 3 [(Estrada 1,[Carro,Nenhum,Carro]),(Relva,[Nenhum,Arvore,Arvore])]))
+to8 = "ForaMapaporCima"   ~: True  ~=? jogoTerminou (Jogo (Jogador (0,(-1))) (Mapa 3 [(Estrada 1,[Carro,Nenhum,Carro]),(Relva,[Nenhum,Arvore,Arvore])]))   
+
+-- Pela Esquerda/Direita:
+
+to9 = "ForaMapaDireita"   ~: True  ~=? jogoTerminou (Jogo (Jogador (3,0)) (Mapa 3 [(Estrada 1,[Carro,Nenhum,Carro]),(Relva,[Nenhum,Arvore,Arvore])]))
+to10= "ForaMapaEsquerda"  ~: True  ~=? jogoTerminou (Jogo (Jogador ((-1),0)) (Mapa 3 [(Estrada 1,[Carro,Nenhum,Carro]),(Relva,[Nenhum,Arvore,Arvore])]))
+
+
 testsT4 :: Test
-testsT4 = TestLabel "Testes Tarefa 4" $ test ["Teste 1" ~: 1 ~=? 1]
+testsT4 = TestLabel "Testes Tarefa 4" $ test [to1,to2,to3,to4,to5,to6,to7,to8,to9,to10]
