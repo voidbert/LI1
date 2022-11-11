620dea68f554492396f98c94d80e321faf37ecc8
a104348 <a104348@alunos.uminho.pt>
2022-11-11 23:31:50 +0000

    Testes unitários de T3
    Juntamente com uma pequena atualização da documentação do módulo.

--- src/Tarefa3_2022li1g012.hs
+++ src/Tarefa3_2022li1g012.hs
@@ -20,7 +20,10 @@ Description : Movimentação do personagem e obstáculos
 Copyright   : José António Fernandes Alves Lopes <a104541@alunos.uminho.pt>
               Humberto Gil Azevedo Sampaio Gomes <a104348@alunos.uminho.pt>
 
-Módulo para a realização da Tarefa 3 do projeto de LI1 em 2022/23.
+Módulo para a realização da Tarefa 3 do projeto de LI1 em 2022/23. Animação de um jogo (jogador e
+mapa) de acordo com os critérios em:
+
+[Especificações](https://gitlab.com/uminho-di/li1/2223/projetos/2022li1g012/-/blob/main/spec/Fase1.pdf)
 -}
 module Tarefa3_2022li1g012 (
   -- * Funções expostas
--- tests/Tarefa3_2022li1g012_Spec.hs
+++ tests/Tarefa3_2022li1g012_Spec.hs
@@ -69,8 +69,108 @@ animaLinha' l (t, o) = if velocidadeTerreno t == 0 then
 l15 = "Composta" ~: True ~=? all
   (\ n -> animaLinha' 5 (Rio n, lrio) == animaLinha 5 (Rio n, lrio)) [-10..10]
 
+
+
+-- Testes dos obstáculos do jogador (onde está e para onde deseja ir)
+
+m = Mapa 5 [(Relva,        [Nenhum, Arvore, Arvore, Nenhum, Arvore]),
+            (Rio 2,        [Tronco, Tronco, Nenhum, Tronco, Tronco]),
+            (Estrada (-1), [Nenhum, Carro,  Carro,  Nenhum, Carro])]
+
+-- Alguns testes com o jogador fora do mapa (OOB - out of bounds)
+oj1 = "OOB1" ~: (Nothing, Nothing) ~=?
+  obstaculosJogador (Jogador (-1, -1)) (Move Baixo) m
+oj2 = "OOB2" ~: (Nothing, Nothing) ~=?
+  obstaculosJogador (Jogador (-1, 3)) (Move Direita) m
+oj3 = "OOB3" ~: (Nothing, Nothing) ~=?
+  obstaculosJogador (Jogador (5, -1)) (Move Cima) m
+oj4 = "OOB4" ~: (Nothing, Nothing) ~=?
+  obstaculosJogador (Jogador (5, 3)) Parado m
+
+-- Jogador fora do mapa a mover-se para o mapa ou vice-versa (IB - in bounds)
+oj5 = "IB1" ~: (Just (Relva, Nenhum), Nothing) ~=?
+  obstaculosJogador (Jogador (0, 0)) (Move Esquerda) m
+oj6 = "IB2" ~: (Nothing, Just (Relva, Arvore)) ~=?
+  obstaculosJogador (Jogador (2, -1)) (Move Baixo) m
+oj7 = "IB3" ~: (Just (Rio 2, Tronco), Nothing) ~=?
+  obstaculosJogador (Jogador (4, 1)) (Move Direita) m
+oj8 = "IB4" ~: (Nothing, Just (Estrada (-1), Carro)) ~=?
+  obstaculosJogador (Jogador (2, 3)) (Move Cima) m
+
+-- Jogador dentro do mapa
+oj9 = "In1" ~: (Just (Rio 2, Tronco), Just (Relva, Arvore)) ~=?
+  obstaculosJogador (Jogador (1, 1)) (Move Cima) m
+oj10 = "In2" ~: (Just (Rio 2, Tronco), Just (Estrada (-1), Carro)) ~=?
+  obstaculosJogador (Jogador (4, 1)) (Move Baixo) m
+oj11 = "In3" ~: (Just (Relva, Nenhum), Just (Relva, Arvore)) ~=?
+  obstaculosJogador (Jogador (3, 0)) (Move Esquerda) m
+oj12 = "In4" ~: (Just (Rio 2, Tronco), Just (Rio 2, Tronco)) ~=?
+  obstaculosJogador (Jogador (0, 1)) (Move Direita) m
+oj13 = "In5" ~: (Just (Estrada (-1), Nenhum), Just (Estrada (-1), Nenhum)) ~=?
+  obstaculosJogador (Jogador (3, 2)) Parado m
+
+-- Testes de animaJogador
+
+-- Colisões com árvores e bordas do mapa
+aj1 = "Col1" ~: Jogador (3, 0) ~=?
+  animaJogador (Jogador (3, 0)) (Move Esquerda) m
+aj2 = "Col2" ~: Jogador (3, 0) ~=?
+  animaJogador (Jogador (3, 0)) (Move Direita) m
+aj3 = "Col3" ~: Jogador (3, 2) ~=?
+  animaJogador (Jogador (3, 2)) (Move Baixo) m
+aj4 = "Col4" ~: (Jogador (0, 2)) ~=?
+  animaJogador (Jogador (0, 3)) (Move Cima) m -- o jogador pode reentrar no mapa
+aj5 = "Col5" ~: (Jogador (5, 5)) ~=?
+  animaJogador (Jogador (5, 5)) (Move Esquerda) m
+
+-- Movimentos laterais em troncos
+aj6 = "Lat1" ~: Jogador (3, 1) ~=?
+  animaJogador (Jogador (1, 1)) Parado m
+aj7 = "Lat2" ~: Jogador (2, 1) ~=?
+  animaJogador (Jogador (1, 1)) (Move Esquerda) m
+aj8 = "Lat3" ~: Jogador (6, 1) ~=?
+  animaJogador (Jogador (4, 1)) Parado m -- pode sair do mapa
+aj9 = "Lat4" ~: Jogador (-2, 0) ~=?
+  -- jogador não afetado pela circularidade
+  animaJogador (Jogador (1, 0)) (Move Direita)
+  (Mapa 3 [(Rio (-4), [Nenhum, Tronco, Tronco])])
+
+-- Movimento vertical
+
+m2 = Mapa 4 [ (Rio (-1), [Nenhum, Tronco, Tronco, Nenhum]),
+              (Rio 2,    [Tronco, Tronco, Nenhum, Tronco]),
+              (Relva,    [Nenhum, Nenhum, Nenhum, Arvore])]
+
+-- Sem colisões
+aj10 = "Vert1" ~: Jogador (1, 1) ~=?
+  animaJogador (Jogador (1, 2)) (Move Cima) m2
+aj11 = "Vert2" ~: Jogador (0, 0) ~=?
+  animaJogador (Jogador (0, 1)) (Move Cima) m2
+aj12 = "Vert3" ~: Jogador (2, 1) ~=?
+  animaJogador (Jogador (2, 0)) (Move Baixo) m2
+-- Com colisões
+aj13 = "Vert4" ~: Jogador (5, 1) ~=?
+  animaJogador (Jogador (3, 1)) (Move Baixo) m2
+aj14 = "Vert5" ~: Jogador (0, 0) ~=?
+  animaJogador (Jogador (1, 0)) (Move Cima) m2
+
+-- animaJogo anima o jogador e o mapa individualmente, algo que já foi testado
+-- em profundidade. Apenas um exemplo:
+
+ma = Mapa 5 [(Relva,        [Nenhum, Arvore, Arvore, Nenhum, Arvore]),
+             (Rio 2,        [Tronco, Tronco, Tronco, Tronco, Nenhum]),
+             (Estrada (-1), [Carro,  Carro,  Nenhum, Carro, Nenhum])]
+
+ajg = "Jogo" ~: Jogo (Jogador (3, 1)) ma ~=?
+  animaJogo (Jogo (Jogador (1, 1)) m) (Move Cima)
+
 testsT3 :: Test
 testsT3 = TestLabel "Testes Tarefa 3" $ test [
   TestLabel "Testes de Linhas individuais" $ test [
-    l1, l2, l3, l4, l5, l6, l7, l8, l9, l11, l12, l13, l14, l15 ]
+    l1, l2, l3, l4, l5, l6, l7, l8, l9, l11, l12, l13, l14, l15 ],
+  TestLabel "Testes de obstaculosJogador" $ test [
+    oj1, oj2, oj3, oj4, oj5, oj6, oj7, oj8, oj9, oj10, oj11, oj12, oj13 ],
+  TestLabel "Testes de animaJogador" $ test [
+    aj1, aj2, aj3, aj4, aj5, aj6, aj7, aj8, aj9, aj10, aj11, aj12, aj13, aj14
+  ], ajg
   ]
