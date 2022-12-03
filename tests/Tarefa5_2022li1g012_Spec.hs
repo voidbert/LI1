7a36d3fbf4635917f8fb097e450f5ce0f7da2373
a104541 <a104541@aluno.uminho.pt>
2022-12-03 21:45:41 +0000

    Testes unitários T5

--- tests/Tarefa5_2022li1g012_Spec.hs
+++ tests/Tarefa5_2022li1g012_Spec.hs
@@ -19,3 +19,28 @@ module Tarefa5_2022li1g012_Spec where
 import LI12223
 import Tarefa5_2022li1g012
 import Test.HUnit
+
+t0 = "Uma linha de Mapa e 0 de valor aleatório"   
+    ~: Jogo (Jogador (0,1)) (Mapa 2 [(Estrada 1,[Carro,Nenhum])])
+    ~=? deslizaJogo 0 (Jogo (Jogador (0,0)) (Mapa 2 [(Estrada 1,[Carro,Nenhum])]))
+
+t1 = "Duas linhas de Mapa"
+    ~: Jogo (Jogador (0,1)) (Mapa 2 [(Estrada 1,[Carro,Nenhum]),
+                                     (Rio 1,[Tronco,Nenhum])])
+    ~=? deslizaJogo 1 (Jogo (Jogador (0,0)) (Mapa 2 [(Rio 1,[Tronco,Nenhum]),
+                                                     (Relva ,[Nenhum,Arvore])]))
+
+-- Verificação do parametro velocidade em 2 Rios consecutivos
+
+t2 = "Dois Rios Seguidos"
+    ~: Jogo (Jogador (0,1)) (Mapa 2 [(Rio (-1),[Tronco,Nenhum]),
+                                     (Rio 1,[Tronco,Nenhum])])
+    ~=? deslizaJogo 3 (Jogo (Jogador (0,0)) (Mapa 2 [(Rio 1,[Tronco,Nenhum]),
+                                                     (Relva ,[Nenhum,Arvore])]))
+
+t3 = "Mapa vazio com potencial de criar uma linha"
+    ~: Jogo (Jogador (0,1)) (Mapa 2 [])
+    ~=? deslizaJogo 0 (Jogo (Jogador (0,0)) (Mapa 2 [])) 
+
+testsT5 :: Test
+testsT5 = TestLabel "Testes Tarefa 5" $ test [t0,t1,t2,t3]
