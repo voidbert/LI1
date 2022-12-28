2f5aaaf2edcad0d52ab2dd5c05063566995606f0
a104541 <a104541@aluno.uminho.pt>
2022-12-28 22:13:58 +0000

    Documentação e adição de variáveis a usar em diferentes módulos

--- src/Audio_2022li1g012.hs
+++ src/Audio_2022li1g012.hs
@@ -1,4 +1,11 @@
-module Audio_2022li1g012 where
+module Audio_2022li1g012 (
+  -- * funções principais
+  pararAudio', comecarAudio', pararAudios,
+  -- * funções auxiliares
+  pararAudio, comecarAudio,
+  -- ** variáveis
+  menuAudio, jogoAudio, goAudio
+) where
 
 import Data.List
 import System.Process
@@ -7,7 +14,13 @@ import System.FilePath
 
 import LI12223
 
+-- | Variáveis para incluir nos diferentes módulos ao invés do Filepath completo. 
+
 menuAudio = "assets/export/AudioPlaylist/Menu.wav" 
+jogoAudio = "assets/export/AudioPlaylist/Jogo.wav"
+goAudio   = "assets/export/AudioPlaylist/GameOver.wav"
+
+-- | 'pararAudio' para um áudio dado um tipo 'Audios' e um FilePath.
 
 pararAudio :: Audios -> FilePath -> IO Audios
 pararAudio ads fp = do
@@ -18,6 +31,8 @@ pararAudio ads fp = do
              (Just (f, ph)) -> do tryIOError $ terminateProcess ph
                                   return $ del f ads
 
+-- | 'comecarAudio' começa um áudio dado um tipo 'Audios' e um FilePath.
+
 comecarAudio :: Audios -> FilePath -> IO Audios
 comecarAudio ads fp = do
   ads' <- pararAudio ads fp -- Parar o som antes de o começar
@@ -25,13 +40,25 @@ comecarAudio ads fp = do
   case h of (Left _)   -> return ads' -- Falha ao iniciar o áudio
             (Right h') -> return ((fp, h') : ads')
 
+{-|
+  'comecarAudio'' permite o uso do construtor 'Assets' na função comecarAudio,
+  para ser possível a adição simples do áudio nos módulos necessários.
+-} 
+
 comecarAudio' :: Assets -> FilePath -> IO Assets 
 comecarAudio' (Assets f t b a) fp = comecarAudio a fp >>= 
   \ x -> return (Assets f t b x)
 
+{-|
+  'pararAudio'' permite o uso do construtor 'Assets' na função pararAudio,
+  para ser possível a remoção simples do áudio nos módulos necessários.
+-} 
+
 pararAudio' :: Assets -> FilePath -> IO Assets
 pararAudio' (Assets f t b a) fp = pararAudio a fp >>= 
   \ x -> return (Assets f t b x)
+
+-- | 'pararAudios' para todos os audios contidos numa lista de Audios. 
 
 pararAudios :: Audios -> IO ()
 pararAudios [] = return ()
