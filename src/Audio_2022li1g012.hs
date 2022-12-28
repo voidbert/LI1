9805e8f5cacf87453bf520b193018dc27e9d3ee3
a104541 <a104541@aluno.uminho.pt>
2022-12-28 20:31:11 +0000

    Versão final das funções para controlo do audio

--- src/Audio_2022li1g012.hs
+++ src/Audio_2022li1g012.hs
@@ -1,70 +1,39 @@
 module Audio_2022li1g012 where
+
+import Data.List
 import System.Process
 import System.IO.Error
+import System.FilePath
 
 import LI12223
 
-data Audios = Audio {
-  menu :: Maybe ProcessHandle,
-  jogo :: Maybe ProcessHandle,
-  gameOver :: Maybe ProcessHandle              
- }
+menuAudio = "assets/export/AudioPlaylist/Menu.wav" 
 
+pararAudio :: Audios -> FilePath -> IO Audios
+pararAudio ads fp = do
+  let ma = find (equalFilePath fp . fst) ads
+      del _ []     = [] -- delete e deleteBy não se aplicam devido ao ProcessHandle
+      del f (x@(fx,_):xs) = if fx `equalFilePath` f then xs else x : del f xs
+  case ma of Nothing  -> return ads -- Áudio não encontrado
+             (Just (f, ph)) -> do tryIOError $ terminateProcess ph
+                                  return $ del f ads
 
-comecarMenu :: Audios -> IO Audios
-comecarMenu a = do
-  -- pararMenu não fará nada se o audio estiver parado. Mas vai pará-lo se estiver a tocar, para
-  -- se recomeçar
-  (Audio m j g) <- pararMenu a
-  h <- tryIOError $ spawnProcess "mpv" ["--loop", "--no-terminal", "AudioPlaylist/Menu.wav"] -- abrir mpv
-  case h of (Left _)  -> return $ Audio Nothing j g -- Falha ao abrir
-            (Right m') -> return $ Audio (Just m') j g -- O mpv abriu
+comecarAudio :: Audios -> FilePath -> IO Audios
+comecarAudio ads fp = do
+  ads' <- pararAudio ads fp -- Parar o som antes de o começar
+  h <- tryIOError $ spawnProcess "mpv" ["--loop", "--no-terminal", fp]
+  case h of (Left _)   -> return ads' -- Falha ao iniciar o áudio
+            (Right h') -> return ((fp, h') : ads')
 
-pararMenu :: Audios -> IO Audios
-pararMenu a@(Audio Nothing j g) = return a -- ^ Não parar o que já está parado
-pararMenu (Audio (Just m) j g) = do
-  tryIOError $ terminateProcess m -- Tentar parar o mpv
-  return $ Audio Nothing j g -- Consiga-se ou não parar, o 1.º argumento é Nothing
+comecarAudio' :: Assets -> FilePath -> IO Assets 
+comecarAudio' (Assets f t b a) fp = comecarAudio a fp >>= 
+  \ x -> return (Assets f t b x)
 
-comecarJogo :: Audios -> IO Audios
-comecarJogo a = do
-  -- pararJogo não fará nada se o audio estiver parado. Mas vai pará-lo se estiver a tocar, para
-  -- se recomeçar
-  (Audio m j g) <- pararJogo a
-  h <- tryIOError $ spawnProcess "mpv" ["--loop", "--no-terminal", "AudioPlaylist/Jogo.wav"] -- abrir mpv
-  case h of (Left _)  -> return $ Audio m Nothing g -- Falha ao abrir
-            (Right j') -> return $ Audio m (Just j') g -- O mpv abriu
+pararAudio' :: Assets -> FilePath -> IO Assets
+pararAudio' (Assets f t b a) fp = pararAudio a fp >>= 
+  \ x -> return (Assets f t b x)
 
-pararJogo :: Audios -> IO Audios
-pararJogo a@(Audio m Nothing g) = return a -- ^ Não parar o que já está parado
-pararJogo (Audio m (Just j) g) = do
-  tryIOError $ terminateProcess j -- Tentar parar o mpv
-  return $ Audio m Nothing g -- Consiga-se ou não parar, o 1.º argumento é Nothing
+pararAudios :: Audios -> IO ()
+pararAudios [] = return ()
+pararAudios (x:xs) = pararAudio (x:xs) (fst x) >>= pararAudios 
 
-
-comecarGO :: Audios -> IO Audios
-comecarGO a = do
-  -- pararJogo não fará nada se o audio estiver parado. Mas vai pará-lo se estiver a tocar, para
-  -- se recomeçar
-  (Audio m j g) <- pararJogo a
-  h <- tryIOError $ spawnProcess "mpv" ["--loop", "--no-terminal", "AudioPlaylist/GameOver.wav"] -- abrir mpv
-  case h of (Left _)  -> return $ Audio m Nothing g -- Falha ao abrir
-            (Right g') -> return $ Audio m j (Just g') -- O mpv abriu
-
-pararGO :: Audios -> IO Audios
-pararGO a@(Audio m j Nothing) = return a -- ^ Não parar o que já está parado
-pararGO (Audio m j (Just g)) = do
-  tryIOError $ terminateProcess g -- Tentar parar o mpv
-  return $ Audio m j Nothing -- Consiga-se ou não parar, o 1.º argumento é Nothing
-
--- Isto é só para testagem. Na prática tens o gloss.
-loopInput :: Audios -> IO ()
-loopInput a = do
-  putStrLn "\n\n\n1) Começar menu\n2) Começar jogo\n3) Começar Game Over\n4) Parar menu\n5) Parar jogo\n6) Parar Game Over\n"
-  c <- getChar
-  case c of '1' -> comecarMenu a >>= loopInput
-            '2' -> comecarJogo a >>= loopInput
-            '3' -> comecarGO   a >>= loopInput
-            '4' -> pararMenu   a >>= loopInput
-            '5' -> pararJogo   a >>= loopInput
-            '6' -> pararGO     a >>= loopInput
